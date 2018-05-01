3118ae5fa0abba1d19043179b6d1905fe44f3ee1
huyanrong <515020842@qq.com>
2018-05-01 23:21:16 +0800

    update

--- demo6/app.jsx
+++ demo6/app.jsx
@@ -30,60 +30,60 @@
 // )
 
 //用keys提取组件（1.错误写法）
-// function ListItem(props) {
-//     const value = props.value;
-//     return (
-//         // 错啦！你不需要在这里指定key:  //  为什么???
-//         <li key={value.toString()}>
-//             {value}
-//         </li>
-//     );
-// }
-  
-// function NumberList(props) {
-//     const numbers = props.numbers;
-//     const listItems = numbers.map((number) =>
-//         //错啦！元素的key应该在这里指定：
-//         <ListItem value={number} />
-//     );
-//     return (
-//         <ul>
-//             {listItems}
-//         </ul>
-//     );
-// }
-  
-//用keys提取组件（2.正确写法）
 function ListItem(props) {
-    // 对啦！这里不需要指定key:
-    return <li>{props.value}</li>;
+    const value = props.value;
+    return (
+        // 错啦！你不需要在这里指定key:  //  与其兄弟节点做比较
+        <li key={value.toString()}>
+            {value}
+        </li>
+    );
 }
   
 function NumberList(props) {
     const numbers = props.numbers;
     const listItems = numbers.map((number) =>
-        // 又对啦！key应该在数组的上下文中被指定
-        <ListItem key={number.toString()} value={number} />
-        );
-        return (
-            <ul>
-            {listItems}
-            </ul>
-        );
-}
-
-function NumberList(props) {
-    const numbers = props.numbers;
-    return (
-      <ul>
-        {numbers.map((number) =>
-          <ListItem key={number.toString()}
-                    value={number} />
-  
-        )}
-      </ul>
+        //错啦！元素的key应该在这里指定：
+        <ListItem value={number} />
     );
-  }
+    return (
+        <ul>
+            {listItems}
+        </ul>
+    );
+}
+  
+//用keys提取组件（2.正确写法）
+// function ListItem(props) {
+//     // 对啦！这里不需要指定key:
+//     return <li>{props.value}</li>;
+// }
+  
+// function NumberList(props) {
+//     const numbers = props.numbers;
+//     const listItems = numbers.map((number) =>
+//         // 又对啦！key应该在数组的上下文中被指定
+//         <ListItem key={number.toString()} value={number} />
+//         );
+//         return (
+//             <ul>
+//             {listItems}
+//             </ul>
+//         );
+// }
+
+// function NumberList(props) {
+//     const numbers = props.numbers;
+//     return (
+//       <ul>
+//         {numbers.map((number) =>
+//           <ListItem key={number.toString()}
+//                     value={number} />
+  
+//         )}
+//       </ul>
+//     );
+//   }
 
 const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
